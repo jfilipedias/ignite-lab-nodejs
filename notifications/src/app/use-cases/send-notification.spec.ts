4703f6d7d2226ffbe4c2217fe989427df5d6140a
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-12-15 18:13:52 -0300

    Starts to implements the notifications repository

--- notifications/src/app/use-cases/send-notification.spec.ts
+++ notifications/src/app/use-cases/send-notification.spec.ts
@@ -1,8 +1,14 @@
 import { SendNotification } from './send-notification';
 
+const notificationsRepository = {
+  async create(notification: Notification) {
+    console.log(notification);
+  },
+};
+
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
     const { notification } = await sendNotification.execute({
       content: 'Você recebeu uma solicitação de amizade.',
       recipientId: 'exemple-recipient-id',
